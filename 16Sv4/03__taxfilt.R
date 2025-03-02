0004a1920f5d9b7516a58cdac306a27814621987
Katie Kyle <katiekyle93@gmail.com>
2025-03-02 09:34:32 -0500

    Update 03__taxfilt.R

--- 16Sv4/03__taxfilt.R
+++ 16Sv4/03__taxfilt.R
@@ -36,48 +36,56 @@ for( i in seq_along(libs) ) {
 
 
 ## ---- filtering taxa ------------------------------------------------------------------- ##
-ps = readRDS(file.path(work.dir,"decontam","ps.p.decontam.prev0.5.RDS"))
-ps.nonc = prune_samples(!(sample_names(ps) %>% grepl(pattern = "NC|NFW")), 
-                        ps)
+f = file.path(work.dir,"decontam","ps.p.decontam.prev0.5.RDS")
+ps = readRDS(f)
+ps.nonc = prune_samples(
+	!(sample_names(ps) %>% grepl(pattern = "NC|NFW")), 
+	ps
+)
+
 # subset_taxa() removes instances of NA if part of match
 ps.nonc = na_to_unclassified_taxa(ps.nonc)
 
 # starting with 2165 taxa and 166 samples
-ps.filt = subset_taxa(ps.nonc, Kingdom == "Bacteria")			# 2158 taxa
+ps.filt = subset_taxa(ps.nonc, Kingdom == "Bacteria")					# 2158 taxa
 ps.filt = subset_taxa(ps.filt, Phylum != "unclass. Bacteria")	# 2150 taxa
-ps.filt = subset_taxa(ps.filt, Order != "Chloroplast")			# 2105 taxa 
-ps.filt = subset_taxa(ps.filt, Family != "Mitochondria")		# 2068 taxa
+ps.filt = subset_taxa(ps.filt, Order != "Chloroplast")				# 2105 taxa 
+ps.filt = subset_taxa(ps.filt, Family != "Mitochondria")			# 2068 taxa
 saveRDS(ps.filt, "./rds/ps.p.filt.RDS")
 
 # make a table of reads lost
-filt.df <- as.data.frame( list(
-  input = ps.nonc %>% 
-    sample_sums(),
-	k.bact = ps.nonc %>% 
-		subset_taxa(Kingdom == "Bacteria") %>% 
-		sample_sums(),
-	p.nna = ps.nonc %>% 
-		subset_taxa(Kingdom == "Bacteria") %>% 
-		subset_taxa(Phylum != "unclass. Bacteria") %>% 
-		sample_sums(),
-	o.nchl = ps.nonc %>% 
-		subset_taxa(Kingdom == "Bacteria") %>% 
-		subset_taxa(Phylum != "unclass. Bacteria") %>% 
-		subset_taxa(Order != "Chloroplast") %>%
-		sample_sums(),
-	f.nmit = ps.nonc %>% 
-		subset_taxa(Kingdom == "Bacteria") %>% 
-		subset_taxa(Phylum != "unclass. Bacteria") %>% 
-		subset_taxa(Order != "Chloroplast") %>%
-		subset_taxa(Family != "Mitochondria") %>%
-		sample_sums()
+filt.df <- as.data.frame( 
+	list(input = 
+			 	ps.nonc %>% sample_sums(),
+			 k.bact = 
+			 	ps.nonc %>% subset_taxa(Kingdom == "Bacteria") %>% 
+			 	sample_sums(),
+			 p.nna = 
+			 	ps.nonc %>% subset_taxa(Kingdom == "Bacteria") %>% 
+			 	subset_taxa(Phylum != "unclass. Bacteria") %>% 
+			 	sample_sums(),
+			 o.nchl = 
+			 	ps.nonc %>% subset_taxa(Kingdom == "Bacteria") %>% 
+			 	subset_taxa(Phylum != "unclass. Bacteria") %>% 
+			 	subset_taxa(Order != "Chloroplast") %>%
+			 	sample_sums(),
+			 f.nmit = 
+			 	ps.nonc %>% subset_taxa(Kingdom == "Bacteria") %>% 
+			 	subset_taxa(Phylum != "unclass. Bacteria") %>% 
+			 	subset_taxa(Order != "Chloroplast") %>%
+			 	subset_taxa(Family != "Mitochondria") %>%
+			 	sample_sums()
 ) )
 write.csv(filt.df, "./csv/filt.df.csv")
 
 
 ## ---- track ASVs through pipeline  ---------------------------------------------------------------------- ##
-seqtab.p = as.data.frame(readRDS("./rds/seqtab.p.RDS")); colnames(seqtab.p) = seq(1,ncol(seqtab.p))
-seqtab.nochim.p = as.data.frame(readRDS("./rds/seqtab.p.nochim.RDS")); colnames(seqtab.nochim.p) = seq(1,ncol(seqtab.nochim.p))
+# UNpooled
+	seqtab.p = as.data.frame(readRDS("./rds/seqtab.p.RDS"))
+	colnames(seqtab.p) = seq(1,ncol(seqtab.p))
+# pooled
+	seqtab.nochim.p = as.data.frame(readRDS("./rds/seqtab.p.nochim.RDS"))
+	colnames(seqtab.nochim.p) = seq(1,ncol(seqtab.nochim.p))
 
 # count number of ASVs per sample
 asv.track = list()
@@ -87,28 +95,25 @@ asv.track$decontam.prev0.1 = rowSums( otu_table(ps.prev0.1) != 0 )
 asv.track$decontam.prev0.25 = rowSums( otu_table(ps.prev0.25) != 0 )
 asv.track$decontam.prev0.5 = rowSums( otu_table(ps) != 0 )			# ps = ps.prev0.5
 ps = na_to_unclassified_taxa(ps)
-asv.track$kng.bact = rowSums(otu_table(
-								ps %>% 
-								subset_taxa(Kingdom == "Bacteria")
-							) != 0 )
-asv.track$phy.nona = rowSums(otu_table( 
-								ps %>% 
-								subset_taxa(Kingdom == "Bacteria") %>% 
-								subset_taxa(Phylum != "unclass. Bacteria")
-							) != 0 )
-asv.track$ord.nchl = rowSums(otu_table( 
-								ps %>% 
-								subset_taxa(Kingdom == "Bacteria") %>% 
-								subset_taxa(Phylum != "unclass. Bacteria") %>%
-								subset_taxa(Order != "Chloroplast")
-							) != 0 )
-asv.track$fam.nmit = rowSums(otu_table( 
-								ps %>% 
-								subset_taxa(Kingdom == "Bacteria") %>% 
-								subset_taxa(Phylum != "unclass. Bacteria") %>%
-								subset_taxa(Order != "Chloroplast") %>%
-								subset_taxa(Family != "Mitochondria")
-							) != 0 )
-write.csv(as.data.frame(asv.track), "./csv/asv.track.csv",quote=F,row.names=T)
+asv.track$kng.bact = 
+	rowSums(otu_table(ps %>% 
+										subset_taxa(Kingdom == "Bacteria") ) != 0 )
+asv.track$phy.nona = 
+	rowSums(otu_table( ps %>% 
+										subset_taxa(Kingdom == "Bacteria") %>% 
+										subset_taxa(Phylum != "unclass. Bacteria") ) != 0 )
+asv.track$ord.nchl = 
+	rowSums(otu_table( ps %>% 
+										subset_taxa(Kingdom == "Bacteria") %>% 
+										subset_taxa(Phylum != "unclass. Bacteria") %>%
+										subset_taxa(Order != "Chloroplast") ) != 0 )
+asv.track$fam.nmit = 
+	rowSums(otu_table( ps %>% 
+										subset_taxa(Kingdom == "Bacteria") %>% 
+										subset_taxa(Phylum != "unclass. Bacteria") %>%
+										subset_taxa(Order != "Chloroplast") %>%
+										subset_taxa(Family != "Mitochondria") ) != 0 )
 
-
+write.csv( as.data.frame(asv.track), 
+					"./csv/asv.track.csv",
+					quote=F,row.names=T )
