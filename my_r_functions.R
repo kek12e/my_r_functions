b1cd22c8911d30b3e11e2eb7fd33ad29ed1eb472
Katie Kyle <katiekyle93@gmail.com>
2025-03-03 09:22:45 -0500

    add make_cultivar_fungus()

--- my_r_functions.R
+++ my_r_functions.R
@@ -178,3 +178,44 @@ na_to_unclassified_taxa <- function(physeq, rank.start=1, glom=0) {
   
   return(ps)
 }
+
+make_cultivar_fungus = function(ps) {
+  ## defines "cultivar fungus" genus
+  ## input: phyloseq object
+  ## return: phyloseq object
+  
+  # check for NA's in Genus names
+  if( all( is.na(tax_table(ps)[,"Genus"]) ) ) {
+    cat(">>> NA's detected in Genus names, running na_to_unclassified_taxa() ... ... ...")
+    ps = na_to_unclassified_taxa(ps)
+  }
+  
+  # get "cultivar fungus" indices
+  w.cf = which( tax_table(ps)[,"Genus"] == "g__Leucocoprinus" |
+                tax_table(ps)[,"Genus"] == "g__Leucoagaricus" |
+                tax_table(ps)[,"Genus"] == "unclassified f__Agaricaceae" )
+  
+  # create "cultivar fungus" fasta for BLASTn confirm
+  if( length(w.cf) > 0 ) {
+    cat(">>> Potential cultivar fungus ASVs detected. Creating fasta ... ... ...\n")
+    
+    # make fasta of "cultivar fungus" ASVs
+    cf.dnass = Biostrings::DNAStringSet(unlist(lapply(refseq(ps)[w.cf],toString)))
+    fa.names = paste0(names(cf.dnass),"_"," ",tax_table(ps)[w.cf,"Genus"])
+    names(cf.dnass) = fa.names
+    
+    # write to file
+    f = file.path(fna.dir,"cultivar_fungus_ASVs.fasta")
+    cat(">>> Writing file", f," ... ... ...\n")
+    cf.dnass %>% Biostrings::writeXStringSet(f, format="fasta")
+
+    # set new genus name
+    tax_table(ps)[w.cf,"Genus"] = "Cultivar Fungus"
+
+    # glom by genus
+    ps = tax_glom(ps, "Genus", NArm=T)
+  }
+  
+  return(ps)
+  
+} #\end of func
